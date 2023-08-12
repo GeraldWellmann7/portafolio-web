6ab910eca8ab90a035c210547652c24b86aaaa58
GeraldWellmann7 <gwellmann1997@gmail.com>
2023-08-11 22:22:38 -0400

    update proyectos

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,5 @@
-
 import tienda from '../assets/tienda-online.png';
+import Login2 from '../assets/Login2.png';
 
 
 export default function Portfolio(){
@@ -20,7 +20,7 @@ export default function Portfolio(){
                   
 
                   <div className="card-work border-1">
-          <div className="m-2 md:w-[500px]">
+          <div className="m-2 w-[400px] md:w-[500px]">
           <a href="https://geraldwellmann7.github.io/tienda-online/" target="_blank" title="HTML link image example">
           <img
               src={tienda}
@@ -42,6 +42,39 @@ export default function Portfolio(){
                   >REACT</span >
                   <span className="decoration-work"
                   >TAILWIND</span >
+              </div>
+              
+            </article>
+          </div>
+
+          
+        </div>
+
+        <div className="card-work border-1">
+          <div className="m-2 w-[400px] md:w-[525px] ">
+          <a href="https://geraldwellmann7.github.io/login-react/" target="_blank" title="HTML link image example">
+          <img
+              src={Login2}
+              alt=""
+              className="opacity-80 hover:opacity-100 "
+            />
+</a>
+            
+            <h4 className="font-bold my-2">TIENDA</h4>
+            <article className="flex justify-between">
+              <div className="flex flex-nowrap items-center">
+                <span className="decoration-work"
+                  >HTML</span    >
+                <span className="decoration-work"
+                  >CSS</span         >
+                <span className="decoration-work"
+                  >JS</span >
+                  <span className="decoration-work"
+                  >REACT</span >
+                  <span className="decoration-work"
+                  >TAILWIND</span >
+                  <span className="decoration-work"
+                  >FIREBASE</span >
               </div>
               
             </article>
